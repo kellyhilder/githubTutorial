20bc919f1cbce924b832a46de552e1501a046794
Kelly Hilder <hilder.kelly@gmail.com>
2020-02-10 11:31:48 -0800

    changed the size of the identity matrix and second matrix

--- myScript.py
+++ myScript.py
@@ -1,9 +1,10 @@
 import numpy as np
+import pandas as pd
 
-my_matrix = np.identity(4)
+my_matrix = np.identity(5)
 # print(my_matrix)
 
-another_matrix = np.random.randint(10, size=(3, 4))
+another_matrix = np.random.randint(10, size=(3, 5))
 print(another_matrix)
 
 sorted_matrix = np.sort(another_matrix, axis=1)
