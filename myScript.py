e6e2a4c0857c35a3e14a9d3087bef42075eeab69
Kelly Hilder <hilder.kelly@gmail.com>
2020-02-10 11:54:05 -0800

    reverted again

--- myScript.py
+++ myScript.py
@@ -1,10 +1,9 @@
 import numpy as np
-import pandas as pd
 
-my_matrix = np.identity(5)
+my_matrix = np.identity(4)
 # print(my_matrix)
 
-another_matrix = np.random.randint(10, size=(3, 5))
+another_matrix = np.random.randint(10, size=(3, 4))
 print(another_matrix)
 
 sorted_matrix = np.sort(another_matrix, axis=1)
