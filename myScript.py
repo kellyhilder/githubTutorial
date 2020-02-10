536bcd6a31e58e59533872934b011b7ca4925010
Kelly Hilder <hilder.kelly@gmail.com>
2020-02-10 10:55:18 -0800

    I added a second matrix and sorted it by row

--- myScript.py
+++ myScript.py
@@ -1,4 +1,10 @@
 import numpy as np
 
 my_matrix = np.identity(4)
-print(my_matrix)
+# print(my_matrix)
+
+another_matrix = np.random.randint(10, size=(3, 4))
+print(another_matrix)
+
+sorted_matrix = np.sort(another_matrix, axis=1)
+print(sorted_matrix)
